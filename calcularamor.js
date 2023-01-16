6fc2eec7aa97cb38655bfb9eb6e6c29988dfd396
BrasilTechAndCo <106916227+crde44@users.noreply.github.com>
2023-01-16 18:21:00 -0300

    Add files via upload
    Calcular porcentagem de amor.

---
+++ calcularamor.js
@@ -0,0 +1,6 @@
+prompt("Seu nome:");
+ prompt("Nome da pessoa:");
+
+ var loveScore = Math.random() * 100;
+ loveScore = Math.floor(loveScore) + 1;
+ alert("A chance de vocês é de " + loveScore + " % de possibilidade.");
